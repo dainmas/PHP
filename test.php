3a827796077ddda89e6816c560352503cd3e521c
dainmas <dainorai@inbox.lt>
2019-09-09 16:20:08 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Aš valdau HTML<?php print 'ir PHP';?></title>
+    </head>
+    <body>
+        <h1>Dainora - HTML asė <?php print 'ir PHP';?></h1>
+        <p>Moku kurti statinį turinį. Atidaryta <?php print date('Y-m-d h:i:sa');?></p>
+    </body>
+</html>
