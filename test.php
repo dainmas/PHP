b07b630aead6dd6b98097ef449f8557dda21c9b2
dainmas <dainorai@inbox.lt>
2019-09-10 15:24:44 +0300

    php.pratybos->paspaudžiam dešiniu klavišu->Git->Commit(paspaudžiam)ir rašom komentarą

--- test.php
+++ test.php
@@ -1,12 +1,19 @@
 <?php
-print date_default_timezone_set('Europe/Vilnius');
+/*
+ * komentaras
+ *
+ date_default_timezone_set('Europe/Vilnius');
+*/
+print 'Labas';
+//print 'iki';
+
 ?>
 <html>
     <head>
         <title>Aš, <?php print date('l'). 'ir PHP';?></title>
     </head>
     <body>
-        <h1>Dainora - HTML  <?php print 'ir PHP';?> asė jau nuo <?php print date('Y-m-d') . ' ' . 'metų';?></h1>
-        <p>Viskas prasidėjo  <?php print date('m') . ' ' . 'mėnesio' .  ',' . date('d'). ' ' . 'dieną'. '!';?></p>
+        <h1>Dainora - HTML  <?php print 'ir PHP';?> asė jau nuo <?php print date('Y-m-d') . ' metų';?></h1>
+        <p>Viskas prasidėjo  <?php print date('m') . ' mėnesio,' . date('d'). ' dieną!';?></p>
     </body>
 </html>
