04c2cbe04612df04abefec05a5d8f018f3a89ba0
dainmas <dainorai@inbox.lt>
2019-09-10 13:00:39 +0300

    pataisiau

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
-
+<?php
+print date_default_timezone_set('Europe/Vilnius');
+?>
 <html>
     <head>
         <title>Aš, <?php print date('l'). 'ir PHP';?></title>
     </head>
     <body>
-        <h1>Dainora - HTML  <?php print 'ir PHP';?> asė jau nuo <?php print date('Y-m-d h:i:sa') . ' ' . 'metų';?></h1>
-        <p>Viskas prasidėjo  <?php print date('m') . 'mėnesio' .  ',' . date('d'). 'dieną'. '!';?></p>
+        <h1>Dainora - HTML  <?php print 'ir PHP';?> asė jau nuo <?php print date('Y-m-d') . ' ' . 'metų';?></h1>
+        <p>Viskas prasidėjo  <?php print date('m') . ' ' . 'mėnesio' .  ',' . date('d'). ' ' . 'dieną'. '!';?></p>
     </body>
 </html>
