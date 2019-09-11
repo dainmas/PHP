382ebedea0329e8eb48d00a3b34ca463687ea487
dainmas <dainorai@inbox.lt>
2019-09-11 12:27:18 +0300

    užduotis su style

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<?php
 
-date_default_timezone_set('Europe/Vilnius');
-
-?>
 <html>
-    <head>
-        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1,10). 'years')). '!'; ?></title>
-    </head>
-    <body>
-        <h1>Dainora - Galbūt turėsiu <?php print rand(1,5); ?> vaikų(us)!</h1>
-        <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2,10) . 'years')); ?></p>
+    <body style = "background-color: rgb(rand(1, 10)">
+        <h1 style = "background-color: rgb(rand(1, 10)">Aš keičiu savo dydį</h1>
+        <p style = "background-color: rgb(rand(1, 10)">Aš keičiu savo spalvą</p>
     </body>
 </html>
