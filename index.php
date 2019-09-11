469ca8b51d2c5cf87e6ff3075ac9c1c02fee3d8e
dainmas <dainorai@inbox.lt>
2019-09-11 12:43:36 +0300

    užduotis su style

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 
 <html>
-    <body style = "background-color: rgb(rand(1, 10)">
-        <h1 style = "background-color: rgb(rand(1, 10)">Aš keičiu savo dydį</h1>
-        <p style = "background-color: rgb(rand(1, 10)">Aš keičiu savo spalvą</p>
+    <head>
+        <meta charset="UTF-8">
+        <title>Rand() + CSS</title>
+    </head>
+    <body style="background-color: rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>)">
+        <h1 style="font-size: <?php print rand(10, 30) . 'px'; ?>">
+            Aš keičiu savo dydį!
+        </h1>
+        <p style="color: rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>)">
+            Aš keičiu savo spalvą!
+        </p>
     </body>
 </html>
