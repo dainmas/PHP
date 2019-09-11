ca54ef8e83182f28a9c4e9ad79efcedb1a13f77a
dainmas <dainorai@inbox.lt>
2019-09-11 11:24:00 +0300

    užduotis su Trampu

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+
 date_default_timezone_set('Europe/Vilnius');
+
 ?>
 <html>
     <head>
-        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+1 day')). '!';?></title>
+        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1,10). 'years')). '!'; ?></title>
     </head>
     <body>
-        <h1>Milda - su manimi buvo ir <?php print date('H', strtotime('-1 hour'));?> valandą</h1>
-        <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų</p>
+        <h1>Dainora - Galbūt turėsiu <?php print rand(1,5); ?> vaikų(us)!</h1>
+        <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2,10) . 'years')); ?></p>
     </body>
 </html>
