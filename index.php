b0034a7fd824558a03190f52783f3c94e63c0089
dainmas <dainorai@inbox.lt>
2019-09-11 13:41:18 +0300

    bombos didejimas

--- index.php
+++ index.php
@@ -2,27 +2,21 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>bomba</title>
+        <title>Bomb img game, and date()</title>
         <style>
-
-            .bomba {
-                background-image: url('https://storage.needpix.com/rsynced_images/atomic-bomb-2026117_1280.png');
-                background-size: contain;
-                width: 1<?php print date('s'); ?>px;                  
+            .bomb-img {
+                background-image: url(http://pngimg.com/uploads/bomb/bomb_PNG16.png);
+                background-size: cover;
+                width: 1<?php print date('s'); ?>px;
                 height: 1<?php print date('s'); ?>px;
-                background-position: center;
-                background-repeat: no-repeat;
             }
-
-            .time-00 {
-                background-image: url('https://image.shutterstock.com/image-illustration/nuclear-bomb-explosion-mushroom-cloud-260nw-1083546056.jpg');   
-            }
-
         </style>
     </head>
     <body>
-        <div class="bomba time-<?php print date('s'); ?>"></div>
-
-        <h1><?php print date('s'); ?> </h1>
+        <div class="bomb-img"></div>
+        <div><?php print date('s'); ?></div>
     </body>
 </html>
+
+
+
