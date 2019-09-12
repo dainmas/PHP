ac5fdbac2cfd762e47861de786cb7f27392c5774
dainmas <dainorai@inbox.lt>
2019-09-12 10:31:15 +0300

    paveiksleliai

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$img = "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+$img = 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
 ?>
 <html>
