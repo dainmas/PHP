9da056c5384e058cd892147621fb8fc23c216f6a
dainmas <dainorai@inbox.lt>
2019-09-12 12:31:48 +0300

    siuksles su skaiciavimu pataisytas

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 $siuksliadezes_turis = 40;
 $siuksliu_turis_per_d = 15;
-$max_kaupo_turis = rand(45, 55);
-$d = $max_kaupo_turis / $siuksliu_turis_per_d;
+$max_kaupo_turis = rand(5, 10);
+$d = ($siuksliadezes_turis + $max_kaupo_turis) / $siuksliu_turis_per_d;
 $date = date('Y-m-d', strtotime('+' . $d . 'days'));
  
 ?>
