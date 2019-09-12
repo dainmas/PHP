e4b7f79630bc7f90bbbe7dc0c6a099593452e390
dainmas <dainorai@inbox.lt>
2019-09-12 10:21:51 +0300

    paveiksleliai

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $img = "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
@@ -10,10 +9,10 @@ $img = "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=co
     </head>
     <body>
         <ul>
-            <li><img src="<?php print $img; ?>"</li>
-            <li><img src="<?php print $img; ?>"</li>
-            <li><img src="<?php print $img; ?>"</li>
-            <li><img src="<?php print $img; ?>"</li>
+            <ol><img src="<?php print $img; ?>"></ol>
+            <ol><img src="<?php print $img; ?>"></ol>
+            <ol><img src="<?php print $img; ?>"></ol>
+            <ol><img src="<?php print $img; ?>"></ol>
         </ul>
     </body>
 
