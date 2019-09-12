943d74166d85c669441ee73c2c9f79c7f3cf2af5
dainmas <dainorai@inbox.lt>
2019-09-12 12:41:11 +0300

    siuksles su skaiciavimu su floor pataisytas

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 $siuksliadezes_turis = 40;
 $siuksliu_turis_per_d = 15;
 $max_kaupo_turis = rand(5, 10);
-$d = ($siuksliadezes_turis + $max_kaupo_turis) / $siuksliu_turis_per_d;
+$d = floor($siuksliadezes_turis + $max_kaupo_turis) / $siuksliu_turis_per_d;
 $date = date('Y-m-d', strtotime('+' . $d . 'days'));
- 
+$text = "Po $d ($date) daryk ką nors, kad išvengtum konflikto.";
+
 ?>
 <html>
     <head>
         <title>Šiukšlės</title>  
     </head>
     <body>
-        <p>Po <?php print floor($d) . ' dienų' . ' ' . $date; ?> daryk ką nors, kad išvengtum konflikto.</p>
+        <p><?php print $text; ?> </p>
     </body>
 </html>
