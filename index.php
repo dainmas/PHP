32f32bb59731b7815df5142463b760af379fbe5d
dainmas <dainorai@inbox.lt>
2019-09-13 12:18:28 +0300

    grizo namo su if geras

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <?php
 
-$title = 'Buitinė skaičiuoklė';
-
 $grizai_velai = rand(0, 1);
-$grizai_isgeręs = rand(0, 1);
-$text_outcome = 'Situacija: ';
-if ($grizai_velai > $grizai_isgeręs) {
-    $text_outcome .= 'grįžai vėlai';
-} elseif (
-        $grizai_velai && $grizai_isgeręs) {
-    $text_outcome .= 'grįžai vėlai ir išgėręs';
-} elseif (
-        $grizai_velai < $grizai_isgeręs) {
-    $text_outcome .= 'grįžai išgėręs';
+$grizai_isgeres = rand(0, 1);
+if ($grizai_velai && !$grizai_isgeres) {
+    $pasekmes = 'Grizai velai';
+    $kur_miegosi = 'Miegosi';
+} else if ($grizai_velai && $grizai_isgeres) {
+    $pasekmes = 'Grizai velai ir isgeres';
+    $kur_miegosi = 'Miegosi';
+} else if (!$grizai_velai && $grizai_isgeres) {
+    $pasekmes = 'Grizai isgeres';
+    $kur_miegosi = 'Miegosi';
 } else {
-    $text_outcome .= 'grįžai išgėręs';
+    $pasekmes = 'Nieko nepadarei';
+    $kur_miegosi = 'Nemiegosi';
 }
-
+$kur_miegosi = 'Išvada: ' . $pasekmes . ' ant sofos!';
 ?>
 <html>
     <head>
-        <title><?php print $title; ?></title>  
+        <title>Buitinė skaičiuoklė</title>  
     </head>
     <body>
-        <h1><?php print $title; ?></h1>
-    <li><h2><?php print $text_outcome; ?></h2></li>
-
-</body>
+        <h1>Buitine skaiciuokle </h1>
+        <h2><?php print $pasekmes; ?></h2>
+        <h3><?php print $kur_miegosi; ?></h3>
+    </body>
 </html>
