33fda5ef02bcd7c2eb81f9400d038bf19eb580f0
dainmas <dainorai@inbox.lt>
2019-09-13 10:26:58 +0300

    benzinas su round($a, 2) perku

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $distance = rand(100, 200);
 $consumption = 7.5;
 $price_1 = 1.3;
+$my_money = 100;
 
 $fuel_total = round(($distance * $consumption / 100), 2);
 $price_trip = round(($fuel_total * $price_1), 2);
@@ -11,6 +12,13 @@ $title = 'Kelionės skaičiuoklė';
 $text_1 = "Nuvažiuota distancija: $distance";
 $text_2 = "Sunaudota $fuel_total l. kuro";
 $text_3 = "Kaina: $price_trip pinigų";
+
+if($my_money >= $price_trip){
+    $perku ='įperkama';
+}else{
+    $perku ='neįperkama';
+}
+$text_4 = "Išvada: Kelionė $perku ";
 
 ?>
 <html>
@@ -23,5 +31,7 @@ $text_3 = "Kaina: $price_trip pinigų";
             <li><?php print $text_2; ?></li>
             <li><?php print $text_3; ?></li>
         </ul>
+        <hr>
+        <p><?php print $text_4; ?></p>
     </body>
 </html>
