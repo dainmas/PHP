85c14977bf20bc40a4d571543381e3d7ba6c3e06
dainmas <dainorai@inbox.lt>
2019-09-13 11:56:28 +0300

    grizo namo su if

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
 
+$title = 'Buitinė skaičiuoklė';
+
 $grizai_velai = rand(0, 1);
 $grizai_isgeręs = rand(0, 1);
-
+$text_outcome = 'Situacija: ';
 if ($grizai_velai > $grizai_isgeręs) {
-    $text_1;
+    $text_outcome .= 'grįžai vėlai';
 } elseif (
         $grizai_velai && $grizai_isgeręs) {
-    $text_2;
+    $text_outcome .= 'grįžai vėlai ir išgėręs';
 } elseif (
         $grizai_velai < $grizai_isgeręs) {
-    $text_3;
+    $text_outcome .= 'grįžai išgėręs';
 } else {
-    $text_4;
+    $text_outcome .= 'grįžai išgėręs';
 }
-$title = 'Buitinė skaičiuoklė';
-$text_1 = "Situacija: grįžai vėlai";
-$text_2 = "Situacija: grįžai vėlai ir išgėręs";
-$text_3 = "Situacija: grįžai išgėręs";
-$text_4 = "Situacija: nieko nepadarei";
 
 ?>
 <html>
@@ -27,10 +24,7 @@ $text_4 = "Situacija: nieko nepadarei";
     </head>
     <body>
         <h1><?php print $title; ?></h1>
-    <li><h2><?php print $text_1; ?></h2></li>
-    <li><h2><?php print $text_2; ?></h2></li>
-    <li><h2><?php print $text_3; ?></h2></li>
-    <li><h2><?php print $text_4; ?></h2></li>
+    <li><h2><?php print $text_outcome; ?></h2></li>
 
 </body>
 </html>
