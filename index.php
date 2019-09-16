0f99f5b6ffa9fa44b8dd1bc29561f50ba8742677
dainmas <dainorai@inbox.lt>
2019-09-16 11:08:38 +0300

    for kates

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
+
 $kates = rand(1, 3);
 $sunys = rand(1, 3);
 $pavyko = rand(0, 1);
+$katasuniai = 0;
 $text = 'Katašunių išeiga';
-$katasuniai = $kates + $pavyko;
+
+for ($k = 1; $k <= $sunys; $k++) {
+    for ($s = 1; $s <= $kates; $s++) {
+        if ($pavyko == true) {
+      $katasuniai++;
+            break;
+        }
+    }
+}
+
 ?>
 <html>
     <head>
@@ -13,12 +24,7 @@ $katasuniai = $kates + $pavyko;
         <div class="container">
             <h1><?php print $text; ?></h1>
             <h2><?php print "Dalyvavo $kates katės ir $sunys šunys"; ?></h2>
-            <?php for ($kates = 1; $kates <= $sunys; $kates++): ?>
-                <?php if ($kates == $pavyko) : ?>
-                    <h3><?php print "Katašunių išeiga: $katasuniai";
-            break; ?></h3>
-                <?php endif; ?>
-            <?php endfor; ?>
+            <h3><?php print "Katašunių išeiga: $katasuniai"; ?></h3>
         </div>
     </body>
 </html>
