5863d1c484699f92e1f8bda0ed82ebbf3d749c83
dainmas <dainorai@inbox.lt>
2019-09-16 12:47:27 +0300

    wallet<0 teisingas

--- index.php
+++ index.php
@@ -8,12 +8,11 @@ for ($month = 1; $month <= 12; $month++) {
     $month_expenses = rand(550, 1000);
     $wallet += $month_income - $month_expenses;
     if ($wallet < 0) {
-        print $month;
+        $text_1 = "Atsargiai, $month mėnesį gali baigtis pinigai!";
         break;
     }
+    $text_1 = "Po $months m., prognozuojamas likutis: $wallet";
 }
-
-$text1 = "Atsargiai, $month mėnesį gali baigtis pinigai!";
 
 ?>
 <html>
@@ -23,7 +22,7 @@ $text1 = "Atsargiai, $month mėnesį gali baigtis pinigai!";
     <body>
         <div class="container">
             <h1>Wallet Forecast</h1>
-            <h2><?php print $text1; ?></h2>
+            <h2><?php print $text_1; ?></h2>
         </div>
     </body>
 </html>
