a69943b57a548620a83677e26d671cde77b827fb
dainmas <dainorai@inbox.lt>
2019-09-17 10:34:59 +0300

    grikiai pataisytas

--- for_loop.php
+++ for_loop.php
@@ -1,16 +1,17 @@
 <?php
 
 $grikiai = 5000;
-
+$prad_grikiai = $grikiai;
 $days = 0;
 for ($d = 0; $d < $grikiai; $d++) {
     $days = date('d', strtotime("+$d days"));
     $per_day = rand(200, 500);
     $grikiai -= $per_day;
+    
 }
 
 $data = date('Y-m-d', strtotime('+' . $days . 'days'));
-$text_1 = "Rasta grikių: $grikiai g.";
+$text_1 = "Rasta grikių: $prad_grikiai g.";
 $text_2 = "Išgyvensiu dar $days dienas, iki $data.";
 ?>
 
