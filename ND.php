f192caa31ac62efea98985bb4000fdff6aaf618c
dainmas <dainorai@inbox.lt>
2019-09-18 15:54:28 +0300

    dienu array su for panaudojimu- pakeiciant vertes $array[$d] ='Church day'

--- ND.php
+++ ND.php
@@ -5,8 +5,10 @@ $array = [];
 for ($d = 0; $d < 7; $d++) {
     $savaites_diena = date('l', strtotime("+$d days"));
     $array[$d] = $savaites_diena;
-    if ('Saturday' == $savaites_diena || 'Sunday' == $savaites_diena) {
-        $array[$d] = 'weekend'; 
+    if ('Saturday' == $savaites_diena) {
+        $array[$d] = 'Sleep day';
+    } elseif ('Sunday' == $savaites_diena) {
+        $array[$d] = 'Church day';
     }
 }
 // $array[$d] = $savaites_diena; susieju savaites dienas su siandiena
