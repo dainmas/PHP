69e170e67ff824b810e3d119862fe2737515f165
dainmas <dainorai@inbox.lt>
2019-09-18 10:47:04 +0300

    daiktai_array

--- array.php
+++ array.php
@@ -7,20 +7,42 @@ $food_array = [
     [
         'kiekis' => 2,
         'dydis' => 'dideli',
-        'pavadinimas' => 'kiausiniai',
+        'pavadinimas' => 'Kiaušiniai',
+    ],
+    [
+        'kiekis' => 1,
+        'dydis' => 'mažas',
+        'pavadinimas' => 'Baklažanas',
     ],
     [
         'kiekis' => 1,
         'dydis' => 'didelis',
-        'pavadinimas' => 'baklazanas',
-    ],
-    [
-        'kiekis' => 1,
-        'dydis' => 'didelis',
-        'pavadinimas' => 'grietines indelis',
+        'pavadinimas' => 'Grietinės indelis',
     ]
 ];
 
 var_dump($food_array);
 
+$daiktai_array = [
+    [
+        'kieno_vardas' => 'Petro',
+        'kieno_pavarde' => 'Pizdžio',
+        'savybe' => 'pamestas',
+        'daiktas' => 'telefonas'
+    ],
+    [
+        'kieno_vardas' => 'Tomo',
+        'kieno_pavarde' => 'Ablomo',
+        'savybe' => 'naudojamas',
+        'daiktas' => 'buljonas'
+    ],
+    [
+        'kieno_vardas' => 'Anos',
+        'kieno_pavarde' => 'Shitkovos',
+        'savybe' => 'pasibaigęs',
+        'daiktas' => 'rulonas'
+    ]
+        ]
+        
 ?>
+
