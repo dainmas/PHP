e921704e564a674299acb991bb7a14583e8c4424
dainmas <dainorai@inbox.lt>
2019-09-18 11:02:28 +0300

    daiktai_array is didziuju raidziu pataisytas

--- array.php
+++ array.php
@@ -5,24 +5,30 @@ $daiktai_array = [
         'kieno_vardas' => 'Petro',
         'kieno_pavarde' => 'Pizdžio',
         'jo_daiktai' => [
-            'savybe' => 'Pamestas',
-            'daiktas' => 'Telefonas'
+            [
+                'savybe' => 'Pamestas',
+                'daiktas' => 'Telefonas'
+            ]
         ]
     ],
     [
         'kieno_vardas' => 'Tomo',
         'kieno_pavarde' => 'Ablomo',
         'jo_daiktai' => [
-            'savybe' => 'Naudojamas',
-            'daiktas' => 'Buljonas'
+            [
+                'savybe' => 'Naudojamas',
+                'daiktas' => 'Buljonas'
+            ]
         ]
     ],
     [
         'kieno_vardas' => 'Anos',
         'kieno_pavarde' => 'Shitkovos',
         'jo_daiktai' => [
-            'savybe' => 'Pasibaigęs',
-            'daiktas' => 'Rulonas'
+            [
+                'savybe' => 'Pasibaigęs',
+                'daiktas' => 'Rulonas'
+            ]
         ]
     ]
 ];
