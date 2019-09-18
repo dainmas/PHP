6d9cac5e0dc08498419a1672ffc2bf315a58e762
dainmas <dainorai@inbox.lt>
2019-09-18 13:18:52 +0300

    foreach count

--- foreach.php
+++ foreach.php
@@ -1,20 +1,27 @@
 <?php
 
-$mano_atmintis = ['Penktadienis', 'Bėgimas', 'Valgymas', 'Filmas', 'Lijo'];
+$mano_atmintis = [
+'Penktadienis', 'Bėgimas', 'Valgymas',
+ 'Filmas', 'Lijo'
+];
+
+$count_mano_atmintis = count($mano_atmintis) - 1;
+$rand_flashback_idx = rand(0, $count_mano_atmintis);
+$flashback_text = "Flashback $rand_flashback_idx : $mano_atmintis[$rand_flashback_idx]";
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>foreach</title>
     </head>
     <body>
         <h1>Kas buvo penktadienį?</h1>
         <h2>Dainoros atmintis</h2>
+        <h3><?php print $flashback_text; ?></h3>
         <ul>
-            <?php foreach ($mano_atmintis as $atmintis): ?>
-                <li><?php print $atmintis; ?></li>
+            <?php foreach ($mano_atmintis as $prisiminimas): ?>
+                <li><?php print $prisiminimas; ?></li>
             <?php endforeach; ?>
         </ul>
-</body>
+    </body>
 </html>
