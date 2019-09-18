c5d4ca42d848d7b452474856c54dc94fa28f2044
dainmas <dainorai@inbox.lt>
2019-09-18 17:12:25 +0300

    dienu array su for panaudojimu-paskaiciavimas darbo valandu

--- ND.php
+++ ND.php
@@ -1,18 +1,22 @@
 <?php
 
 $array = [];
-
+$total_work_hours = 0;
+$work_hours_per_day = 8;
 for ($d = 0; $d < 7; $d++) {
     $savaites_diena = date('l', strtotime("+$d days"));
 
     if ('Saturday' === $savaites_diena) {
-        $array[$savaites_diena] = 'Sleep day';
+        $array[$savaites_diena] = 'Weekend';
+        $work_hours_per_day .= '';
     } elseif ('Sunday' === $savaites_diena) {
-        $array[$savaites_diena] = 'Church day';
+        $array[$savaites_diena] = 'Very Weekend';
     } else {
         $array[$savaites_diena] = 'Workday';
-    }
+        $total_work_hours += $work_hours_per_day;   
+    } 
 }
+ var_dump($total_work_hours);
 //$arr[$newkey] = $arr[$oldkey];
 //unset($arr[$oldkey]);
 //
