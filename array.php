31f4c1edac909a14f9a949c5e7f8c1e18d99ea9f
dainmas <dainorai@inbox.lt>
2019-09-18 11:29:40 +0300

    lentynu array pataisiau

--- array.php
+++ array.php
@@ -1,45 +1,42 @@
 <?php
 
 $daiktai_array = [
-    [
-        'lentyna_1' => [
-            [
-                'name' => 'Kiaušiniai',
-                'quantity' => 3
-            ],
-            [
-                'name' => 'Grietinė',
-                'quantity' => 1
-            ],
-            [
-                'name' => 'Jogurtas',
-                'quantity' => 2
-            ],
-            [
-                'name' => 'Žuvis',
-                'quantity' => 0
-            ]
+    'lentyna_1' => [
+        [
+            'name' => 'Kiaušiniai',
+            'quantity' => 3
         ],
-        'lentyna_2' => [
-            [
-                'name' => 'Pienas',
-                'quantity' => 1
-            ],
-            [
-                'name' => 'Kefyras',
-                'quantity' => 0
-            ],
-            [
-                'name' => 'Degtinė',
-                'quantity' => 2
-            ],
-            [
-                'name' => 'Kečupas',
-                'quantity' => 2
-            ],
+        [
+            'name' => 'Grietinė',
+            'quantity' => 1
         ],
-    ]
+        [
+            'name' => 'Jogurtas',
+            'quantity' => 2
+        ],
+        [
+            'name' => 'Žuvis',
+            'quantity' => 0
+        ]
+    ],
+    'lentyna_2' => [
+        [
+            'name' => 'Pienas',
+            'quantity' => 1
+        ],
+        [
+            'name' => 'Kefyras',
+            'quantity' => 0
+        ],
+        [
+            'name' => 'Degtinė',
+            'quantity' => 2
+        ],
+        [
+            'name' => 'Kečupas',
+            'quantity' => 2
+        ],
+    ],
 ];
 
 var_dump($daiktai_array);
-
