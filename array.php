c24702dc2b8bb33aa97814a0bde8d1c564d77598
dainmas <dainorai@inbox.lt>
2019-09-19 17:14:06 +0300

    i masyva ikeliam dienotvarke, keiciam, istrinam. $week_day[key] =$dienotvarke

--- array.php
+++ array.php
@@ -10,10 +10,25 @@ $week_day = [
     'Sunday' => 'Išeiginė'
 ];
 
+$dienotvarke = [
+    'Keliamės 6:00',
+    'Sporto klubas',
+    'Rytinis kamštis',
+    'Code Academy',
+    'Vakarinis kamštis',
+    'Netflix'
+];
+
 foreach ($week_day as $key => $value) {
     if ($key === 'Friday') {
-        print 'Friday yra gera diena';
+        $dienotvarke[5] = 'baro linksmybes';
+        $week_day[$key] = $dienotvarke;
+    } elseif ($value === 'Darbo diena') {
+        $week_day[$key] = $dienotvarke;
     }
 }
 
+foreach ($week_day as $key => $value) {
+    unset($week_day[$key]);
+}
 var_dump($week_day);
