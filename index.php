26ff9ad8edb554e8a9da0c24a2c046bf25a2dac4
dainmas <dainorai@inbox.lt>
2019-09-23 21:37:44 +0300

    natos su Aurimu

--- index.php
+++ index.php
@@ -1,85 +1,49 @@
 <?php
-
-$days = 365;
-$pack_price = 3.5;
-
-$count_ttl = 0;
-$count_mon_thu_ttl = 0;
-$time_per_cig = 5;
-
-for ($d = 0; $d < $days; $d++) {
-    $savaites_diena = date('N', strtotime("+$d days"));
-    if ($savaites_diena <= 4) {
-        $cigs_mon_thu = rand(3, 4);
-        $count_ttl += $cigs_mon_thu;
-        $count_mon_thu_ttl += $cigs_mon_thu;
-    } elseif ($savaites_diena == 5) {
-        $cigs_fri = rand(3, 4);
-        $count_ttl += $cigs_fri;
-    } elseif ($savaites_diena == 6) {
-        $cigs_sat = rand(10, 20);
-        $count_ttl += $cigs_sat;
-    } else {
-        $cigs_sun = rand(1, 3);
-        $count_ttl += $cigs_sun;
-    }
+$natos = [
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'A',
+	'B'
+];
+$song = [];
+$natu_suma = count($natos);
+for ($i = 0; $i < 3; $i++) {
+	$root = rand(0, $natu_suma - 1);
+	
+	for ($j = 0; $j < 3; $j++, $root += 2) {
+		if ($root >= $natu_suma) {
+			$note = $root - $natu_suma;
+			$song[$i][$note] = $natos[$note];
+		} else {
+			$note = $root;
+			$song[$i][$note] = $natos[$note];
+		}
+	}
 }
-//for ($c = 0; $c < $count_ttl; $c++) {
-//    if ($c % 20 == 0) {
-//        $class = 'img-border';
-//    } else {
-//        $class = 'img';
-//    }
-//}
-$number_packs = ceil($count_ttl / 20);
-$number_packs_mon_thu = ceil($count_mon_thu_ttl / 20);
-$price_ttl = $number_packs * 3.5;
-$price_mon_thu = $number_packs_mon_thu * 3.5;
-$time_total = round($count_ttl * $time_per_cig / 60);
-
-$text_1 = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
-$text_2 = "Nerūkydamas darbo dienomis, sutaupyčiau $price_mon_thu eur.";
-$text_3 = "Viso traukdamas prastovėsiu $time_total valandų.";
-
+var_dump($song);
 ?>
 <html>
-    <head>
-        <title>Dumai</title>
-        <style>
-            .img{
-                background-image: url(https://previews.123rf.com/images/kuzma/kuzma0603/kuzma060300104/339315-one-cigarette.jpg);
-                background-size: cover;
-                width: 40px;
-                height: 80px;
-                display: inline-block;
-
-            }
-
-            .img-border{
-                border: 2px solid red;
-                width: 900px;
-                height: 85px;
-                margin:10px 0px;;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Mano dūmų skaičiuoklė</h1>
-            <h2><?php print $text_1; ?></h2>
-            <h3><?php print $text_2; ?></h3>
-            <h3><?php print $text_3; ?></h3>
-        </div>
-        <div class="container-img">
-            <?php for ($c = 0; $c < $count_ttl; $c++) : ?>
-                <?php if ($c % 20 == 0) : ?>
-                    <div class="img-border">
-                        <div class="img"></div>
-                    <?php else: ?>
-                        <div class="img"></div>
-                    <?php endif; ?>
-                <?php endfor; ?>
-            </div>
-        </div>
-    </body>
+<head>
+	<title>Song</title>
+	<style>
+		.key {
+			height: 200px;
+			width: 50px;
+			border: 1px solid black;
+			display: inline-block;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<?php foreach ($natos as $nata): ?>
+			<div class="key">
+				<?php print $nata; ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+</body>
 </html>
