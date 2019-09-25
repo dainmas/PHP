9ac44ce5f67efeb7eba5a29c77e6dabca7d96a31
dainmas <dainorai@inbox.lt>
2019-09-25 16:52:39 +0300

    slot_run funkcija 3 X 3 sugeneruotas kubikas

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * 
+ * @param type $size
+ * @return type
+ */
 function slot_run($size) {
     $array = [];
 
@@ -11,6 +16,63 @@ function slot_run($size) {
 
     return $array;
 }
+
 $slot_machine = slot_run(3);
 
 var_dump($slot_machine);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>foreach</title>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            .row {
+
+
+            }
+            .column{
+                border: 1px solid grey;
+                height: 50px; 
+            }
+            .orange{
+                width:100px;
+                height: 100px;
+                border:1px solid grey;
+
+                display:inline-block;
+                background-color: orange;
+            }
+
+            .blue{
+                width:100px;
+                height: 100px;
+                border:1px solid grey;
+                display:inline-block;
+                background-color: blue;
+            }
+
+
+        </style>
+    </style>
+</head>
+<body>
+    <h1>slot run</h1>
+    <div class="container">
+        <?php foreach ($slot_machine as $row) : ?>
+            <div class="row"> 
+                <?php foreach ($row as $column) : ?>
+                <?php if($column == 1): ?>
+                    <div class="<?php print 'orange' ?>"></div>
+                    <?php else: ?>
+                    <div class="<?php print 'blue' ?>"></div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </div> 
+        <?php endforeach; ?>
+    </div>
+</body>
+</html>
