960520881e7f219c366f122f42660bebba4409c9
dainmas <dainorai@inbox.lt>
2019-09-27 16:40:53 +0300

    reference & apie avis surištas ir atrištas

--- rekursija.php
+++ rekursija.php
@@ -1,10 +1,32 @@
 <?php
 
 $x = 0;
+$b = &$x;
+//panaikinam reference:
+unset($b);
+$b = 1;
 
-function change_x(&$x) {
-    $x = 1;
+print $x;
+
+
+$roll_joints = true;
+$joint1 = &$roll_joints;
+$joint2 = &$joint1;
+$joint3 = &$joint2;
+
+print $joint1 . $joint2 . $joint3 . '<br>';
+
+$sheep = ['bee'];
+//for ciklas surišantis avis
+for ($i = 0; $i < 4; $i++) {
+    $sheep[] = &$sheep[$i];
 }
 
-change_x($x);
-print $x;
+//vieną avį atrišau
+foreach ($sheep as $key => $value) {
+    unset($sheep[$key]);
+    $sheep[$key] = $value;
+}
+
+$sheep[3] = 'Aš kitokia!';
+var_dump($sheep);
