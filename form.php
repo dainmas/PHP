a60f9bc0f03a3c7f047fa5fee938a7a35577bfc7
dainmas <dainorai@inbox.lt>
2019-09-30 21:32:10 +0300

    form pilnai POST

--- form.php
+++ form.php
@@ -1,37 +1,89 @@
 <?php
-
 var_dump($_POST);
-$value = 0;
+$date = date('Y-m-d');
+$anketa = [];
 
 if (isset($_POST['enter'])) {
-    $value = $_POST['enter'];
-    $value ++;
+    $anketa['name'] = 'Name: ' . $_POST['name'];
+    $anketa['last-name'] = 'Last Name: ' . $_POST['last-name'];
+    $anketa['age'] = 'Age: ' . $_POST['age'];
+    $anketa['level'] = 'Level: ' . $_POST['level'];
+    $anketa['text'] = 'Žmogus vardu, ' . $_POST['name'] . ' ' . $_POST['last-name'] . ', kuriam(-iai) yra ' . $_POST['age'] . ' metai,  yra PHP ' . $_POST['level'] . '.<br><br> Anketa užpildyta ' . $date . '.';
 }
-
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Form</title>
         <style>
-            .banana{
-                background-image: url('https://i5.walmartimages.com/asr/209bb8a0-30ab-46be-b38d-58c2feb93e4a_1.1a15fb5bcbecbadd4a45822a11bf6257.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF');
-                background-size:cover;
-                width: 150px;
-                height: 150px;
-                display:inline-block;
-                margin:5px;
+            .form_container{
+                width: 50%;
+                height: 50%;
+                margin: auto;
+                border: 1px solid red;
+                text-align: left;
+                padding: 10px;
             }
+            .container{
+                width: 50%;
+                margin: auto;
+                border: 1px solid red;
+                text-align: left;
+                padding: 10px;
+                background-image: url('https://previews.123rf.com/images/vladimirs/vladimirs1612/vladimirs161200029/69152035-old-wrinkled-paper-background-papers-folds-wrinkles-texture-brown-weathered-parchment.jpg');
+                background-size: cover;
+            }
+
+            .certificate{
+                width: 200px;
+            }
+
+            h1{
+                text-align: center;
+            }
+
         </style>
     </head>
     <body>
-        <form method="POST">
-            <span>Give it to me!</span>
-            <input type="submit" name="enter" value="<?php print $value; ?>"/>
-            <span>*clic*</span>
-        </form>
-        <?php for($i =0; $i < $value; $i++): ?>
-            <div class="banana"></div>
-        <?php endfor; ?>
+        <?php if (empty($_POST)): ?>
+            <div class="form_container">
+                <h1>Užpildyk formą:</h1>
+                <form method="POST">
+                    <label>
+                        <span>Name:</span>
+                        <input type="text" name="name" value="" placeholder="Your Name" required>
+                    </label> 
+                    <br>
+                    <br>
+                    <label>
+                        <span>Last Name:</span>
+                        <input type="text" name="last-name" value="" placeholder="Your Last Name" required>
+                    </label>
+                    <br>
+                    <br>
+                    <label>
+                        <span>Age:</span>
+                        <input type="age" name="age" value="" placeholder="Enter your age">
+                    </label>
+                    <br>
+                    <br>
+                    <label for="level">Kaip vertini savo PHP žinias:</label>
+                    <select name="level">
+                        <option value="pradedantysis" selected>Pradedantysis</option>
+                        <option value="pazenges" selected>Pažengęs</option>
+                        <option value="profesionalas" selected>Profesionalas</option>
+                    </select>
+                    <input type="submit" name="enter" value="Paspausti">
+                </form>
+            </div>
+            <!--Sertifikatas-->
+        <?php else: ?>
+            <div class="container">
+                <img class="certificate" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd7Epnq04u_D5EyMEH5BT_aICdGMRGL-_OcS3LCBYp0XJqHwAW9Q" alt="certificate">
+                <?php foreach ($anketa as $value): ?>
+                    <h2><?php print $value; ?></h2>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
     </body>
 </html>
