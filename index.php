39e2dfa09e6a009c98b2fc09bfb39d1c2cd8f54c
dainmas <dainorai@inbox.lt>
2019-10-08 12:46:16 +0300

    form su validatoriais Pilna

--- index.php
+++ index.php
@@ -7,41 +7,76 @@ $form = [
     'title' => 'Kalėdų norai',
     'fields' => [
         'first_name' => [
-            'attr' => [
-                'type' => 'text'
-            ],
+            'type' => 'text',
+            'value' => '',
             'extra' => [
                 'attr' => [
-                    'placeholder' => 'Aurimas',
+                    'placeholder' => 'Enter your name',
                     'class' => 'input-text',
                     'id' => 'first-name'
                 ]
             ],
             'label' => 'Vardas:',
 //            'error' => 'Vardas per trumpas!'
-            'validate' => [
-                'validate_not_empty'
+            'validators' => [
+                'validate_not_empty',
             ]
         ],
         'last_name' => [
-            'attr' => [
-                'type' => 'text'
-            ],
+            'type' => 'text',
+            'value' => '',
             'extra' => [
                 'attr' => [
-                    'placeholder' => 'Stecenka',
+                    'placeholder' => 'Enter your last name',
                     'class' => 'input-text',
                     'id' => 'last-name'
                 ]
             ],
             'label' => 'Pavardė:',
 //            'error' => 'Paliktas tuščias laukas!'
+            'validators' => [
+                'validate_not_empty',
+            ]
+        ],
+        'age' => [
+            'type' => 'number',
+            'value'=> '',
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Enter your age',
+                    'class' => 'input-text',
+                    'id' => 'age'
+                ]
+            ],
+            'label' => 'Age:',
+//            'error' => 'Paliktas tuščias laukas!'
+            'validators' => [
+                'validate_not_empty',
+                'validate_is_number',
+                'validate_is_positive',
+                'validate_max_120'
+            ]
+        ],
+        'email' => [
+            'type' => 'email',
+            'value' => '',
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Enter your email',
+                    'class' => 'input-text',
+                    'id' => 'email'
+                ]
+            ],
+            'label' => 'Email:',
+//            'error' => 'Paliktas tuščias laukas!'
+            'validators' => [
+                'validate_not_empty',
+                'validate_is_email'
+            ]
         ],
         'wish' => [
-            'attr' => [
-                'type' => 'select',
-                'value' => 'car'
-            ],
+            'type' => 'select',
+            'value' => 'car',
             'extra' => [
                 'attr' => [
                     'class' => 'input-select',
@@ -54,6 +89,9 @@ $form = [
                 'socks' => 'Kojinių'
             ],
             'label' => 'Kalėdom noriu:',
+            'validators' => [
+                'validate_not_empty',
+            ]
         ]
     ],
     'buttons' => [
@@ -66,8 +104,20 @@ $form = [
             'value' => 'Išvalyti'
         ]
     ],
-    'message' => 'Formos Message!'
+    'message' => 'Formos Message!',
+    'callbacks' => [
+        'success' => 'form_success',
+        'fail' => 'form_fail'
+    ]
 ];
+
+function form_success($filtered_input, $form) {
+    var_dump('Success');
+}
+
+function form_fail($filtered_input, $form) {
+    var_dump('fail');
+}
 
 /**
  * Generates HTML attributes
@@ -106,26 +156,80 @@ function validate_not_empty($field_input, &$field) {
     }
 }
 
-function validate_form(&$form) {
-    $filtered_input = get_form_input($form);
+function validate_is_number($field_input, &$field) {
+    if (!is_numeric($field_input)) {
+        $field['error'] = 'Turi būti įrašytas skaičius!';
+    } else {
+        return true;
+    }
+}
 
-    if (!empty($filtered_input)) {
+function validate_is_positive($field_input, &$field) {
+    if ($field_input <= 0) {
+        $field['error'] = 'Privalo būti teigiamas skaičius!';
+    } else {
+        return true;
+    }
+}
 
-        foreach ($form['fields'] as $field_id => &$field) {
-            $field_input = $filtered_input[$field_id];
-            $field['attr']['value'] = $field_input;
-            
-            if (isset($field['validate'])) {
-                foreach ($field['validate'] as $value) {
-                    $value($field_input, $field);
+function validate_max_120($field_input, &$field) {
+    if ($field_input > 120) {
+        $field['error'] = 'Skaičius turi būti mažesnis, negu 120!';
+    } else {
+        return true;
+    }
+}
+
+function validate_is_string($field_input, &$field) {
+    if (!is_string($field_input)) {
+        $field['error'] = 'Įrašykite raides!';
+    } else {
+        return true;
+    }
+}
+
+function validate_is_email($field_input, &$field) {
+    if (!filter_var($field_input, FILTER_VALIDATE_EMAIL)) {
+        $field['error'] = 'Neteisingas emailo formatas!';
+    } else {
+        return true;
+    }
+}
+
+function validate_form($filtered_input, &$form) {
+    $success = true;
+    foreach ($form['fields'] as $field_id => &$field) {
+        $field_input = $filtered_input[$field_id];
+        $field['value'] = $field_input;
+
+        if (isset($field['validators'])) {
+            foreach ($field['validators'] as $validator) {
+                $is_valid = $validator($field_input, $field);
+                if (!$is_valid) {
+                    $success = false;
+                    break;
                 }
             }
         }
     }
+
+    if ($success) {
+        if (isset($form['callbacks']['success'])) {
+            $form['callbacks']['success']($filtered_input, $form);
+        }
+    } else {
+        if (isset($form['callbacks']['fail'])) {
+            $form['callbacks']['fail']($filtered_input, $form);
+        }
+    }
+
+    return $success;
 }
 
-validate_form($form);
-var_dump($form);
+$filtered_input = get_form_input($form);
+if ($filtered_input) {
+    validate_form($filtered_input, $form);
+}
 ?>
 <html>
     <head>
--- uzduotys.php
+++ uzduotys.php
@@ -1,18 +1,39 @@
 <?php
-
-$ingredients = [
-    'obuolys',
-    'miltai',
-    'cukrus',
-    'pienas',
+$array = [
+    'participants' => [
+        [
+            'name' => 'Juozas',
+            'surname' => 'Juozaitis',
+            'age' => 86
+        ],
+        [
+            'name' => 'Dalia',
+            'surname' => 'Zemkalnyte',
+            'age' => 28
+        ],
+        [
+            'name' => 'Mantas',
+            'surname' => 'Britkus',
+            'age' => 41
+        ],
+    ]
 ];
 
-$receptai = [];
+$new_array = [];
+foreach ($array['participants'] as $key => $value) {
+        $new_array[] = $value['age'];
+    }
+
+var_dump($new_array);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
 
 
-foreach ($ingredients as $ingredient) {
-    
-     $receptai['pyragas'][]= $ingredient ;
-     
-}
-var_dump($receptai);
