6ef73c1ea8bbfd64d0f774ef1caecbbfff71c992
dainmas <dainorai@inbox.lt>
2019-10-11 15:10:55 +0300

    Cookie visit

--- bandymai.php
+++ bandymai.php
@@ -17,27 +17,28 @@
 $id = rand(1, 999999);
 $apsilankymai = 0;
 
-
 if (!empty($_COOKIE)) {
     $apsilankymai = $_COOKIE['apsilankymai'] + 1;
     $id = $_COOKIE['user_id'];
 
 }
-
-
+var_dump($_COOKIE['apsilankymai']);
 setcookie('user_id', $id, strtotime('+ 30d'), '/');
 setcookie('apsilankymai', $apsilankymai, strtotime('+ 30d'), '/');
 var_dump($_COOKIE);
+
+$h1_text = "User ID: $id";
+$h2_text = "Visits: $apsilankymai";
 ?>
 
 <html></html>
 <head>
     <meta charset="UTF-8">
-    <title>Form Security</title>
+    <title>Cookie Visits Tracking</title>
     <link rel="stylesheet" href="includes/style.css">
 </head>
 <body>
-    <?php print "User ID: $id"; ?>
-    <?php print "Apsilankymai:  $apsilankymai"; ?>
+    <?php print $h1_text; ?>
+    <?php print $h2_text; ?>
 </body>
 </html>
